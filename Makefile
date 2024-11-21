0164391adfa7c0d3e8337684cd22e68b907f9b8d
rsuntk <rissu.ntk@gmail.com>
2024-11-22 00:10:47 +0700

    Do rissu changes
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- Makefile
+++ Makefile
@@ -311,7 +311,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/x86/ -e s/x86_64/x86/ \
 # Default value for CROSS_COMPILE is not to prefix executables
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 ARCH            ?= arm64
-CROSS_COMPILE   ?= $(srctree)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
+CROSS_COMPILE   ?= aarch64-linux-androidkernel-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
@@ -371,9 +371,8 @@ HOST_LOADLIBES := $(HOST_LFS_LIBS)
 
 # Make variables (CC, etc...)
 AS		= $(CROSS_COMPILE)as
-LD		= $(CROSS_COMPILE)ld
-LDGOLD		= $(CROSS_COMPILE)ld.gold
-CC		= $(srctree)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+LD		= ld.lld
+CC		= clang
 CPP		= $(CC) -E
 AR		= $(CROSS_COMPILE)ar
 NM		= $(CROSS_COMPILE)nm
@@ -482,7 +481,7 @@ endif
 
 ifeq ($(cc-name),clang)
 ifneq ($(CROSS_COMPILE),)
-CLANG_TRIPLE	?= $(srctree)/toolchain/clang/host/linux-x86/clang-r383902/bin/aarch64-linux-gnu-
+CLANG_TRIPLE	?= aarch64-linux-gnu-
 CLANG_FLAGS	+= --target=$(notdir $(CLANG_TRIPLE:%-=%))
 ifeq ($(shell $(srctree)/scripts/clang-android.sh $(CC) $(CLANG_FLAGS)), y)
 $(error "Clang with Android --target detected. Did you specify CLANG_TRIPLE?")
--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,107 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
+#
+# Rissu's kernel build script.
+# For A04+e
+#
+
+On_Red='\033[41m'         # Red
+On_Yellow='\033[43m'      # Yellow
+BBlack='\033[1;30m'       # Black
+BWhite='\033[1;37m'       # White
+On_Blue='\033[44m'        # Blue
+Color_Off='\033[0m'       # Text Reset
+
+KERNELSU_REPO="https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh"
+
+pr_info() {
+	echo -e "${On_Blue}${BWhite}[  INFO  ]${Color_Off} $@"
+}
+pr_warn() {
+	echo -e "${On_Yellow}${BBlack}[  WARN  ]${Color_Off} $@"
+}
+pr_err() {
+	echo -e "${On_Red}${BWhite}[  ERROR  ]${Color_Off} $@"
+}
+
+if [ -z $CROSS_COMPILE ]; then
+	pr_err "Invalid empty variable for \$CROSS_COMPILE"
+elif [ -z $PATH ]; then
+	pr_err "Invalid empty variable for \$PATH"
+elif [ -z $DEFCONFIG ]; then
+	pr_err "Invalid empty variable for \$DEFCONFIG"
+fi
+
+if [[ "$KERNELSU" = "true" ]]; then
+	curl -LSs $KERNELSU_REPO | bash -s main
+else
+	pr_info "KernelSU is disabled, export KERNELSU=true to enable it"
+fi
+
+export CC=clang
+export LD=ld.lld
+export KERNEL_OUT=$(pwd)/out
+
 export ARCH=arm64
-export ANDROID_MAJOR_VERSION=r
+export ANDROID_MAJOR_VERSION=t
+export PLATFORM_VERSION=13
 
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y m32_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+DATE=$(date +'%Y%m%d%H%M%S')
+IMAGE="$KERNEL_OUT/arch/$ARCH/boot/Image"
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+if [ -z $JOBS ]; then
+	JOBS=$(nproc --all)
+fi
+
+# we use bt folder for reference
+if [ -d $(pwd)/drivers/misc/mediatek/connectivity/bt ]; then
+	export TARGET_PRODUCT="a04"
+fi
+
+# Make flags
+MKFLAG="-C $(pwd) --jobs $JOBS O=$KERNEL_OUT CC=clang LD=ld.lld KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y"
+
+build() {
+	if [[ $@ = "defconfig" ]]; then
+		make `echo $MKFLAG` `echo $DEFCONFIG`
+	elif [[ $@ = "kernel" ]]; then
+		make `echo $MKFLAG`
+	else
+		pr_err "Usage: mka defconfig/kernel"
+	fi
+}
+
+if [ -d $KERNEL_OUT ]; then
+	pr_warn "An out/ folder detected, Do you wants dirty builds? (y/N)"
+	read -p "" OPT;
+	
+	if [ $OPT = 'y' ] || [ $OPT = 'Y' ]; then
+		build kernel;
+	else
+		rm -rR out;
+		make clean;
+		make mrproper;
+		build defconfig && build kernel;
+	fi
+else
+	build defconfig && build kernel;
+fi
+
+if [ -e $IMAGE ]; then
+	pr_info "Build done."
+	if [ -d $(pwd)/AnyKernel3 ]; then
+		if [ ! -z $DEVICE ]; then
+			DEVICE_MODEL="`echo $DEVICE`-"
+		fi
+		cp $IMAGE AnyKernel3/
+		cd AnyKernel3 && zip -r6 ../`echo $DEVICE_MODEL`AnyKernel3_`echo $DATE`.zip *
+	 	if [[ $IS_CI != "true" ]]; then
+	  		rm Image && cd ..
+		fi
+	fi
+else
+	pr_err "Build error."
+fi
